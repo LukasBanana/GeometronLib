2262b5d2f2d5b4606e94ee432f80ce8e05ecb352
Lukas Hermanns <lhlukash23@googlemail.com>
2015-08-20 21:16:06 +0200

    Flipped all normals in the frustum.

--- include/Geom/Frustum.h
+++ include/Geom/Frustum.h
@@ -52,52 +52,53 @@ class FrustumT : private ConvexHullT<T>
 
         void SetFromMatrix(const Gs::Matrix4T<T>& m)
         {
+            /* Setup planes with negative equations, so that all normals point out of the convex hull */
             GetPlane(FrustumPlane::Near) =
             {
-                m(2, 0),
-                m(2, 1),
-                m(2, 2),
-                m(2, 3)
+                -( m(2, 0) ),
+                -( m(2, 1) ),
+                -( m(2, 2) ),
+                -( m(2, 3) )
             };
 
             GetPlane(FrustumPlane::Left) =
             {
-                m(3, 0) + m(0, 0),
-                m(3, 1) + m(0, 1),
-                m(3, 2) + m(0, 2),
-                m(3, 3) + m(0, 3)
+                -( m(3, 0) + m(0, 0) ),
+                -( m(3, 1) + m(0, 1) ),
+                -( m(3, 2) + m(0, 2) ),
+                -( m(3, 3) + m(0, 3) )
             };
 
             GetPlane(FrustumPlane::Right) =
             {
-                m(3, 0) - m(0, 0),
-                m(3, 1) - m(0, 1),
-                m(3, 2) - m(0, 2),
-                m(3, 3) - m(0, 3)
+                -( m(3, 0) - m(0, 0) ),
+                -( m(3, 1) - m(0, 1) ),
+                -( m(3, 2) - m(0, 2) ),
+                -( m(3, 3) - m(0, 3) )
             };
 
             GetPlane(FrustumPlane::Top) =
             {
-                m(3, 0) - m(1, 0),
-                m(3, 1) - m(1, 1),
-                m(3, 2) - m(1, 2),
-                m(3, 3) - m(1, 3)
+                -( m(3, 0) - m(1, 0) ),
+                -( m(3, 1) - m(1, 1) ),
+                -( m(3, 2) - m(1, 2) ),
+                -( m(3, 3) - m(1, 3) )
             };
 
             GetPlane(FrustumPlane::Bottom) =
             {
-                m(3, 0) + m(1, 0),
-                m(3, 1) + m(1, 1),
-                m(3, 2) + m(1, 2),
-                m(3, 3) + m(1, 3)
+                -( m(3, 0) + m(1, 0) ),
+                -( m(3, 1) + m(1, 1) ),
+                -( m(3, 2) + m(1, 2) ),
+                -( m(3, 3) + m(1, 3) )
             };
 
             GetPlane(FrustumPlane::Far) =
             {
-                m(3, 0) - m(2, 0),
-                m(3, 1) - m(2, 1),
-                m(3, 2) - m(2, 2),
-                m(3, 3) - m(2, 3)
+                -( m(3, 0) - m(2, 0) ),
+                -( m(3, 1) - m(2, 1) ),
+                -( m(3, 2) - m(2, 2) ),
+                -( m(3, 3) - m(2, 3) )
             };
 
             /* Normalize plane normal vectors */
