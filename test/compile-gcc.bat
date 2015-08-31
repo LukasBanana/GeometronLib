7da74631240780397dc29e938eac45eb3e206ed0
Lukas Hermanns <lhlukash23@googlemail.com>
2015-08-31 16:30:29 +0200

    Modified compile-gcc.bat script.

--- test/compile-gcc.bat
+++ test/compile-gcc.bat
@@ -1,3 +1,4 @@
 g++ -I ..\include -I ..\..\..\GaussianLib\repository\include -std=c++11 -c ..\sources\MeshGeneratorCuboid.cpp ..\sources\TriangleMesh.cpp
-ar rvs gausslib.a TriangleMesh.o MeshGeneratorCuboid.o
-g++ -I ..\include -I ..\..\..\GaussianLib\repository\include -std=c++11 gausslib.a test1.cpp -o test1-gcc.exe
+ar rcs libgeomlib.a TriangleMesh.o MeshGeneratorCuboid.o
+g++ -I ..\include -I ..\..\..\GaussianLib\repository\include -std=c++11 -c test1.cpp -o test1.o
+g++ -std=c++11 test1.o -L .\ -lgeomlib -o test1-gcc.exe
